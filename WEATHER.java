98d490b0465d2c9cdf01f53653c7c7709b92709e
AM71113363 <71590903+AM71113363@users.noreply.github.com>
2025-03-11 07:35:46 +0100

    Update WEATHER.java

--- WEATHER.java
+++ WEATHER.java
@@ -9,80 +9,71 @@ import android.util.*;
 
 public class WEATHER extends View
 {
-	private Bitmap bm;
-	private Bitmap br;
+    private Bitmap bm;
+    private Bitmap br;
     private Paint paint;
-	private Matrix matrix=new Matrix();
+    private Matrix matrix=new Matrix();
     private float hPa=0.0f;
-	private String altitude="0m";
-	private float adjustAltitude=0;
+    private String altitude="0m";
+    private float adjustAltitude=0;
 	
-	private float getAngle()
-	{
-		float pr=hPa + adjustAltitude;
-		if(pr<=990){ return -90; }
-		if(pr>=1020){ return 90; }
-		//range 990 to 1020
-		pr=(pr-990) * 6;
-	    pr-=90;
-		return pr;
-	}
-	
-    
-	public WEATHER(Context ctx, AttributeSet attr)
-	{
-		super(ctx,attr);
-		bm=BitmapFactory.decodeResource(getResources(),R.drawable.back);
-		br=BitmapFactory.decodeResource(getResources(),R.drawable.arm);
-		paint=new Paint();
-		paint.setColor(Color.WHITE);
-		paint.setTextSize(50);
-	}
-
-	@Override
-	protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec)
-	{
-		if(bm==null)
-		{
-		super.onMeasure(widthMeasureSpec, heightMeasureSpec);
-	    }else{
-		setMeasuredDimension(bm.getWidth(),bm.getHeight());
-		}
-	}
-    
-
+    private float getAngle()
+    {
+        float pr=hPa + adjustAltitude;
+	if(pr<=990){ return -90; }
+	if(pr>=1020){ return 90; }
+	//range 990 to 1020
+	pr=(pr-990) * 6;
+        pr-=90;
+      return pr;
+    }
+    public WEATHER(Context ctx, AttributeSet attr)
+    {
+        super(ctx,attr);
+	bm=BitmapFactory.decodeResource(getResources(),R.drawable.back);
+	br=BitmapFactory.decodeResource(getResources(),R.drawable.arm);
+	paint=new Paint();
+	paint.setColor(Color.WHITE);
+	paint.setTextSize(50);
+    }
     @Override
-    protected void onDraw(Canvas canvas) {
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec)
+    {
+        if(bm==null)
+	{
+	    super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+	}else{
+	    setMeasuredDimension(bm.getWidth(),bm.getHeight());
+	}
+    }
+    @Override
+    protected void onDraw(Canvas canvas)
+    {
         super.onDraw(canvas);
+        if(bm!=null)
+	{
+	    canvas.drawBitmap(bm,0,0,null);
+        }
+	canvas.drawText(String.format("%.2f", hPa),170,370,paint);
+	canvas.drawText(altitude,170,440,paint);
 
-		if(bm!=null)
-		{
-			canvas.drawBitmap(bm,0,0,null);
-
-		}
-		canvas.drawText(String.format("%.2f", hPa),170,370,paint);
-		canvas.drawText(altitude,170,440,paint);
-
-		if(br!=null && hPa > 0.0f)
-		{
-			matrix.reset();
-			matrix.postTranslate(-br.getWidth()/2f,-br.getHeight()/2f);
-			matrix.postRotate(getAngle(),0,0);
-			matrix.postTranslate(br.getWidth()/2,br.getHeight()/2);
-			canvas.drawBitmap(br,matrix,null);
-		}
-
+	if(br!=null && hPa > 0.0f)
+	{
+	    matrix.reset();
+	    matrix.postTranslate(-br.getWidth()/2f,-br.getHeight()/2f);
+	    matrix.postRotate(getAngle(),0,0);
+	    matrix.postTranslate(br.getWidth()/2,br.getHeight()/2);
+	    canvas.drawBitmap(br,matrix,null);
 	}
+    }
     public void update(float hpa)
-	{
-		hPa = hpa;
-		invalidate();
-	}
-	public void update(float a,String s)
-	{
-		altitude=s;
-		adjustAltitude = a/8.3f;
-	}
-
+    {
+        hPa = hpa;
+	invalidate();
+    }
+    public void update(float a,String s)
+    {
+        altitude=s;
+	adjustAltitude = a/8.3f;
+    }
 }
-
